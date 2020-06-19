9666e593410568ee47e4959ca7f17c876c02c4f1
lucascmreis <reislcr.rj@gmail.com>
2020-06-18 23:15:14 -0300

    config knexfile

--- knexfile.ts
+++ knexfile.ts
@@ -11,9 +11,20 @@ module.exports = {
       directory: path.resolve(__dirname,'src','database','migrations')
     },
     seeds:{
-      directory: path.resolve(__dirname,'src','database','seeds')
+      directory: path.resolve(__dirname,'src','database','seeds', 'development')
     },
     useNullAsDefault:true
+  },
+
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    migrations: {
+        directory:  path.resolve(__dirname,'src','database','migrations')
+    },
+    seeds: {
+        directory: path.resolve(__dirname,'src','database','seeds', 'production')
+    },
   },
 
 };
